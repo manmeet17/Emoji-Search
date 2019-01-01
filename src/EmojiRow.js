7ab510316cc1b8b4b660f9e014e11c31a9f93fc6
Manmeet Tarun <meetutarun16@gmail.com>
2019-01-01 18:27:11 +0530

    Rows fixed

--- src/EmojiRow.js
+++ src/EmojiRow.js
@@ -6,10 +6,9 @@ class EmojiRow extends Component{
         const codePointHex = this.props.sym.codePointAt(0).toString(16);
         const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
         return (
-            <div className="emoji-res-row">
+            <div className="emoji-res-row" >
                 <img alt={this.props.title} src={src} />
                 <span className="title">{this.props.title}</span>
-                {/* <span className="info">Click to copy emoji</span> */}
             </div>
         );
     }
